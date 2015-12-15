bf72d2f873f0da37369708329d0e63eba6213e73
Giorgio <giopbm7@hotmail.it>
2015-12-15 17:25:55 +0100

    functions in Model, in view and in controller

--- js/app.js
+++ js/app.js
@@ -8,17 +8,27 @@ var SantaModel = {
   /* Initializes the model with a list of requests, 
    * and sets the first one as the current one 
    */
+    current:-1,
    init : function(list){
+       this.requests = list;
+       this.current = 0;
+       
+       
    },
   
    /* It moves "current" to the next request */
    next : function (){
+       this.current++;
    },
   
    /* Returns the current request. 
     * If all requests have been processed (there is no current one), it returns null 
     */
    getCurrentRequest : function () {
+
+       
+       if(this.current== requests.length){return null;}
+       else{return       this.requests[this.current];}
    },  
     
    /* Packs the given item if it fulfills the current request.       
@@ -29,3 +39,58 @@ var SantaModel = {
    }      
   
 };
+
+var SantaController = {
+    init: function(){
+         SantaModel.init(requests);
+        SantaView.init();
+    },
+    current_request: function(){
+        return SantaModel.getCurrentRequest();
+    },
+    next: function(){
+        SantaModel.next();
+    }
+};
+
+var SantaView = {
+    corect: 0,
+    init: function(){
+        var request  = SantaController.current_request();
+        $(".question").html(request.question);
+        for(var i =0; i<request.options.length;i++){
+            $(".question-items").append("<li>"+request.options[i]+"</li>");
+        }
+        
+        $(".question-items li").click(function(){
+            if($(this).text() === request.answer){
+            this.correct++;
+            
+            }
+            SantaController.next();
+            $(".question-items").empty();
+            SantaView.next();
+            
+        })
+    },
+    next: function(){
+        
+        var request  = SantaController.current_request();
+        $(".question").html(request.question);
+        for(var i =0; i<request.options.length;i++){
+            $(".question-items").append("<li>"+request.options[i]+"</li>");
+        }
+        
+    },
+    final: function(){
+   
+    }
+    
+};
+
+
+
+$(document).ready(function(){
+    SantaController.init();
+
+});
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -3,8 +3,25 @@
 
 describe("Santa", function() {
 
-  it("should...", function() {  
-    
-  });    
+  it("getCurrentRequest should work fine", function() {  
+    SantaModel.init(requests);
+    var value1 = SantaModel.getCurrentRequest();
+    SantaModel.next();
+    var value2 = SantaModel.getCurrentRequest();
+
+
+    expect(value1).toBe(requests[0]);
+    expect(value2).toBe(requests[1]);
+  });
+    it("pack should work fine", function() {  
+    SantaModel.init(requests);
+    var value1 = pack("no");
+    SantaModel.next(); 
+    var value2 = pack("no");
+
+    expect(value1).toBe(false);
+    expect(value2).toBe(true);
+
+  });  
  
 });
